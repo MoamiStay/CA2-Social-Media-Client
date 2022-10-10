8477ec2aad6ff15151aef9d01b9456b1b4dde494
Regine <reginenaas@gmail.com>
2022-10-10 21:33:13 +0200

    gjort alt for mye for én commit.
    Register user + Authenticate with token and log in. Fetched posts and listed out on main-feed section on index.html

--- js/register.js
+++ js/register.js
@@ -1,12 +1,63 @@
 /*
+xxx get values from form*
 
-register new user 
-
-get values from form*
+ register new user 
 
 validation (from utils.mjs)
 @noroff.no / @stud.noroff.no email***
 username: no spaces***
 double check password 
-
 */
+
+const getUser = document.querySelector("input#validationDefaultUsername");
+const getEmail = document.querySelector("input#validationDefaultEmail");
+const getPass = document.querySelector("input#validationDefault01");
+// const confirmPass = document.querySelector("input#validationDefault02");
+const submitButton = document.querySelector("button#submit-user");
+const API_BASE_URL = "https://nf-api.onrender.com";
+const registerUrl = `${API_BASE_URL}/api/v1/social/auth/register`;
+
+
+
+async function registerUser(url, userData) {
+    console.log(userData);
+    try {
+        const postData = {
+            method: "POST",
+            headers: {
+                "content-Type": "application/json",
+            },
+            body: JSON.stringify(userData),
+        };
+        const response = await fetch(url, postData);
+        console.log(response);
+        const json = await response.json();
+        console.log(json);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log("You clicked submit");
+
+const username = getUser.value.trim();
+const email = getEmail.value.trim();
+const password = getPass.value.trim();
+console.log(username, email, password);
+
+const userToRegister = {
+    name: username,
+    email: email,
+    password: password,
+}
+
+registerUser(registerUrl, userToRegister);
+});
+
+
+
+/*
+ eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NzkxL…E3MX0.DwXoJZuqJnLAYcr2Al16hRNcVM2Fx0_cMtuJ4sH7qLg
+ */
