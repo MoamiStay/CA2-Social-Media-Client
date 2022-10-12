79318348831e6a0c9b96d3f64640073bd6304b30
Regine <reginenaas@gmail.com>
2022-10-12 17:42:20 +0200

    get profile my posts

--- js/posts.js
+++ js/posts.js
@@ -1,5 +1,5 @@
 const content = document.querySelector("div.post-container");
-const profileContent = document.querySelector("div#profile-container");
+// const profileContent = document.querySelector("div#profile-container");
 const postBtn = document.querySelector("#post-button");
 const textArea = document.querySelector("#message");
 const postTitle = document.querySelector("#post-title");
@@ -9,62 +9,37 @@ const createPostUrl = `https://nf-api.onrender.com/api/v1/social/posts`;
 let posts = "";
 const deleteBtn = document.querySelector("#delete-button");
 
-
 async function getWithToken(url) {
-    try {
-        const token = localStorage.getItem("accessToken");
-        const fetchOptions = {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`,
-            },
-        };
-        const response = await fetch(url, fetchOptions);
-        // console.log(response);
-        const json = await response.json();
-        posts = json;
-        // console.log(json);
-    } catch (error) {
-        console.log(error);
-    }
-    try {
-        mainContent.style.display = "block";
-        header.style.display = "block";
-        loginForm.style.display = "none";
-    } catch (error) {
-        console.log(error);
-    }
-    try {
-        // console.log(posts);
-        for(item of posts) {
-          const myName = localStorage.getItem("userName");
-          if (item.author.name !== myName) {
-            content.innerHTML += `
-            <div class="mb-5 col-lg-6 card">
-            <div class="card-body">
-              <div class="pf-image-sm align-items-center p-1">
-                <img
-                  src="images/profilepic.png"
-                  class="col card-img-top"
-                  alt="profile_image"
-                />
-                <h5 class="col card-title p-2">${item.author.name}</h5>
-                <p>${item.created}</p>
-              </div>
-              <a href="../post.html?id=${item.id}">
-              <p class="card-text">${item.body}</p>
-              </a>
-            </div>
-            <div class="card-body">
-              <a href="#" class="card-link">like</a>
-              <a href="#" class="card-link">comment</a>
-              <a href="#" class="card-link">Save</a>
-            </div>
-          </div>
-            `
-        } else if (item.author.name === myName) { // move this section to profile
-        content.innerHTML += `
+  try {
+      const token = localStorage.getItem("accessToken");
+      const fetchOptions = {
+          method: "GET",
+          headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+          },
+      };
+      const response = await fetch(url, fetchOptions);
+      // console.log(response);
+      const json = await response.json();
+      posts = json;
+      // console.log(json);
+  } catch (error) {
+      console.log(error);
+  }
+  try {
+      mainContent.style.display = "block";
+      header.style.display = "block";
+      loginForm.style.display = "none";
+  } catch (error) {
+      console.log(error);
+  }
+  try {
+      // console.log(posts);
+      for(item of posts) {
+        const myName = localStorage.getItem("userName");
+        if (item.author.name !== myName) {
+          content.innerHTML += `
           <div class="mb-5 col-lg-6 card">
           <div class="card-body">
             <div class="pf-image-sm align-items-center p-1">
@@ -87,13 +62,39 @@ async function getWithToken(url) {
           </div>
         </div>
           `
-        }
+      } else if (item.author.name === myName) { // move this section to profile
+      content.innerHTML += `
+        <div class="mb-5 col-lg-6 card">
+        <div class="card-body">
+          <div class="pf-image-sm align-items-center p-1">
+            <img
+              src="images/profilepic.png"
+              class="col card-img-top"
+              alt="profile_image"
+            />
+            <h5 class="col card-title p-2">${item.author.name}</h5>
+            <p>${item.created}</p>
+          </div>
+          <a href="../post.html?id=${item.id}">
+          <p class="card-text">${item.body}</p>
+          </a>
+        </div>
+        <div class="card-body">
+          <a href="#" class="card-link">like</a>
+          <a href="#" class="card-link">comment</a>
+          <a href="#" class="card-link">Save</a>
+        </div>
+      </div>
+        `
       }
-    } catch (error) {
-        console.log(error);
     }
+  } catch (error) {
+      console.log(error);
+  }
 };
+
 getWithToken(postsUrl);
+
 
 function getPfImg() {
   if (item.author.avatar === "") {
--- js/profile.js
+++ js/profile.js
@@ -1,7 +1,9 @@
 const username = localStorage.getItem("userName");
 const myName  = document.querySelector("#my-name");
 const title = document.querySelector("title");
+const profileContent = document.querySelector("#profile-container");
 const profileUrl = `https://nf-api.onrender.com/api/v1/social/profiles/${username}?_posts=true&_following=true&_followers=true`;
+const postsUrl = `https://nf-api.onrender.com/api/v1/social/posts/?_author=true&_comments=true&_reactions=true`;
 
 async function getUser(url) {
     try {
@@ -25,3 +27,66 @@ async function getUser(url) {
 };
 
 getUser(profileUrl);
+
+
+async function getWithToken(url) {
+    try {
+        const token = localStorage.getItem("accessToken");
+        const fetchOptions = {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        };
+        const response = await fetch(url, fetchOptions);
+        // console.log(response);
+        const json = await response.json();
+        posts = json;
+        // console.log(json);
+    } catch (error) {
+        console.log(error);
+    }
+    try {
+        // profileContent.style.display = "block";
+        // header.style.display = "block";
+        // loginForm.style.display = "none";
+    } catch (error) {
+        console.log(error);
+    }
+    try {
+        // console.log(posts);
+        for(item of posts) {
+          const myName = localStorage.getItem("userName");
+          if (item.author.name === myName) {
+        profileContent.innerHTML += `
+          <div class="mb-5 col-lg-6 card">
+          <div class="card-body">
+            <div class="pf-image-sm align-items-center p-1">
+              <img
+                src="images/profilepic.png"
+                class="col card-img-top"
+                alt="profile_image"
+              />
+              <h5 class="col card-title p-2">${item.author.name}</h5>
+              <p>${item.created}</p>
+            </div>
+            <a href="../post.html?id=${item.id}">
+            <p class="card-text">${item.body}</p>
+            </a>
+          </div>
+          <div class="card-body">
+            <a href="#" class="card-link">like</a>
+            <a href="#" class="card-link">comment</a>
+            <a href="#" class="card-link">Save</a>
+          </div>
+        </div>
+          `
+        }
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  };
+
+  getWithToken(postsUrl);
